46f4d317612c9438080d8d5a4b74f2eb4c506161
Cássio Iribarrem <cassiobernardes96@gmail.com>
2020-07-31 16:19:00 -0300

    Adds fizzBuzz function

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,8 +1,14 @@
-def fizzbuzz(number):
-    if number == 15:
-        return 'FizzBuzz'
-    elif number % 3 == 0:
-        return 'Fizz'
-    elif number % 5 == 0:
-        return 'Buzz'
-    return number
+def transform(number):
+    result = ''
+    if number % 3 == 0:
+        result += 'Fizz'
+    if number % 5 == 0:
+        result += 'Buzz'
+    if number % 3 != 0  and number % 5 != 0:
+        result = number
+    return result
+
+
+def fizzBuzz(last_number):
+    for number in range(1, last_number+1):
+        print(transform(number))
--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -1,29 +1,42 @@
-from fizzbuzz import fizzbuzz
+from fizzbuzz import transform, fizzBuzz
 
 
 def test_should_return_1_given_1():
-    assert 1  == fizzbuzz(1)
+    assert 1  == transform(1)
 
 
 def test_should_return_2_given_2():
-    assert 2  == fizzbuzz(2)
+    assert 2  == transform(2)
 
 
 def test_should_return_fizz_given_3():
-    assert 'Fizz' == fizzbuzz(3)
+    assert 'Fizz' == transform(3)
 
 
 def test_should_return_buzz_given_5():
-    assert 'Buzz' == fizzbuzz(5)
+    assert 'Buzz' == transform(5)
 
 
 def test_should_return_fizz_given_6():
-    assert 'Fizz' == fizzbuzz(6)
+    assert 'Fizz' == transform(6)
 
 
 def test_should_return_buzz_given_10():
-    assert 'Buzz' == fizzbuzz(10)
+    assert 'Buzz' == transform(10)
 
 
 def test_should_return_fizzbuzz_given_15():
-    assert 'FizzBuzz' == fizzbuzz(15)
+    assert 'FizzBuzz' == transform(15)
+
+
+def test_should_return_fizzbuzz_given_30():
+    assert 'FizzBuzz' == transform(30)
+
+
+def test_fizzbuzz_should_print_number_till_15(capsys):
+    fizzBuzz(15)
+    captured = capsys.readouterr()
+    expected_output = '1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\nFizzBuzz\n'
+
+    assert captured.out == expected_output
+
